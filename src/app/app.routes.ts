01bc18ebf7e49485f8840c1c33e31b0b7a0427c8
sousaz <gabrielsousa2003gsa@gmail.com>
2024-01-30 23:39:55 -0300

    feat: the routes files was configured

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: "", pathMatch: "full", redirectTo: "rent" },
+  { path: "rent", loadChildren: () => import("./rent/rent.routes").then(m => m.RENT_ROUTES) },
+];
---
+++ src/app/rent/rent.routes.ts
@@ -0,0 +1,6 @@
+import { RentComponent } from './rent/rent.component';
+import { Routes } from '@angular/router';
+
+export const RENT_ROUTES: Routes = [
+  { path: "", component: RentComponent }
+]
