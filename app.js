c7bd7df3775ebce808dee31fa8f2d3b0369a95a5
sousaz <gabrielsousa2003gsa@gmail.com>
2024-01-30 23:16:44 -0300

    feat: electron has been installed and configured

---
+++ app.js
@@ -0,0 +1,20 @@
+const { app, BrowserWindow } = require("electron")
+
+let appWindow
+
+function  createWindow() {
+  appWindow = new BrowserWindow({
+    width: 1000,
+    height: 800
+  })
+  appWindow.loadFile("dist/rental-manager/browser/index.html")
+
+  appWindow.on("closed", function(){
+    appWindow = null
+  })
+}
+
+app.whenReady().then(() => {
+  createWindow()
+})
+
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'rantal-manager' title`, () => {
+  it(`should have the 'rental-manager' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('rantal-manager');
+    expect(app.title).toEqual('rental-manager');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, rantal-manager');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, rental-manager');
   });
 });
